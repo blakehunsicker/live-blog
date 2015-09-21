ff3ee259466497481a17c3f05b8eee107899bc2b
blake <blakehunsicker@gmail.com>
2015-09-21 10:32:11 -0400

    adding key

--- live.js
+++ live.js
@@ -1,9 +1,13 @@
 $(document).ready(function() {
+
 	$('#liveblog-story-list').css({
-		'width': '60%',
+		'width': '70%',
 		'float': 'right'
 	});
+
 	$('#liveblog-load-more').css({
 		'float': 'right'
 	});
+
+	$('#liveblog-permalink').after('<div class="supplement-key" style="width:25%;height:200px;float:left;background-color:#666;"></div>')
 });
