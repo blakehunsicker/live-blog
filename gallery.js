c56c4a81b3b3475eed69a789d6dd3437c440cc3c
blake <blakehunsicker@gmail.com>
2015-09-22 11:16:41 -0400

    getting closer

--- gallery.js
+++ gallery.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
-	$('.headline-kicker').css('display','none');
-	// $('#article-topper').children('h1').css('display','none');
+	// $('.headline-kicker').css('display','none');
+	$('h1').innerHTML = 'The massacre in Sousse';
 	$('.wp-volt-gal').css('display','none');
 	$('.pb-f-sharebars-utilities-tool-bar').css('display','none');
-	$("<p style='font-size:18px;padding-right:10%;padding-left:10%;'>A gunman opened fire on beachgoers at a resort frequented by Europeans in what is widely seen as a campaign to devastate Tunisia's tourism industry.</p><p style='font-size:18px;padding-right:10%;padding-left:10%;'>Many of the dead and injured were from Britain.</p>").appendTo('.pb-f-gallery-gallery');
+	$("<p style='font-size:18px;width:60%;'>A gunman opened fire on beachgoers at a resort frequented by Europeans in what is widely seen as a campaign to devastate Tunisia's tourism industry.</p><p style='font-size:18px;width:60%;'>Many of the dead and injured were from Britain.</p>").appendTo('.pb-f-gallery-gallery');
 });
