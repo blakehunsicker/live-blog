8694c917f90c59c15095fc2e1e174a7c13ca5450
blake <blakehunsicker@gmail.com>
2015-09-22 10:08:31 -0400

    adding supp content

--- gallery.js
+++ gallery.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
 	$('.headline-kicker').css('display','none');
 	// $('#article-topper').children('h1').innerHTML = 'What is the Islamic State?';
-	$('#wp-volt-gal').css('display','none');
+	$('.wp-volt-gal').css('display','none');
+	$('<h4>Hello hello hello</h4>').appendTo('.pb-f-gallery-gallery');
 });
