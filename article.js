6da699f07a5d922f2b6ecb218dcdec1a31bd1f72
blake <blakehunsicker@gmail.com>
2015-09-22 14:04:57 -0400

    working article.js

--- article.js
+++ article.js
@@ -1,5 +1,7 @@
 $(document).ready(function() {
-	$('.pb-f-sharebars-utilities-tool-bar').css('display','none');
+	// $('.pb-f-sharebars-utilities-tool-bar').css('display','none');
 	$('.pb-sig-line').css('display','none');
+	$('.flex-embed').css('display','none');
+	$('article').children().css('display','none');
 
 });
