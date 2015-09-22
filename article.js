39b2e219fe759059deee5ad552aa842ba0477dba
blake <blakehunsicker@gmail.com>
2015-09-22 16:28:47 -0400

    adding the dot

--- article.js
+++ article.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
-	// $('.pb-f-sharebars-utilities-tool-bar').css('display','none');
+	$('.headline-kicker').children('a').css('display','none');
+	$('<div><i class="fa fa-circle" style="color:#DCA48D;float:left"></i> Supplement explorer</div>')
 	$('#article-topper').children('h1').css('display','none');
 	$('<h1>The attack in Sousse, Tunisia (what would this look like after a line break?)</h1>').appendTo('#article-topper');
 	$('.pb-f-sharebars-utilities-tool-bar').css('display','none');
