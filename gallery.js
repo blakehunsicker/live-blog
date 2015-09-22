33f63cf7194d18a166029b502db9ebc997270ad4
blake <blakehunsicker@gmail.com>
2015-09-22 11:35:52 -0400

    changing title

--- gallery.js
+++ gallery.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
 	// $('.headline-kicker').css('display','none');
-	$('h1').innerHTML = 'The massacre in Sousse';
+	$('#article-topper').children('h1').css('display','none');
+	$('<h1>The Massacre in Sousse</h1>').appendTo('#article-topper');
 	$('.wp-volt-gal').css('display','none');
 	$('.pb-f-sharebars-utilities-tool-bar').css('display','none');
 	$("<p style='font-size:18px;width:60%;padding-left:10px;'>A gunman opened fire on beachgoers at a resort frequented by Europeans in what is widely seen as a campaign to devastate Tunisia's tourism industry.</p><p style='font-size:18px;width:60%;padding-left:10px;'>Many of the dead and injured were from Britain.</p>").appendTo('.pb-f-gallery-gallery');
