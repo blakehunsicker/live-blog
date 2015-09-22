620c61f49263551b9cdb1dd9c80fff4ecd9687cf
blake <blakehunsicker@gmail.com>
2015-09-22 14:06:45 -0400

    title

--- article.js
+++ article.js
@@ -1,5 +1,7 @@
 $(document).ready(function() {
 	// $('.pb-f-sharebars-utilities-tool-bar').css('display','none');
+	$('#article-topper').children('h1').css('display','none');
+	$('<h1>The attack in Sousse, Tunisia</h1>').appendTo('#article-topper');
 	$('.pb-sig-line').css('display','none');
 	$('.flex-embed').css('display','none');
 	$('article').children().css('display','none');
