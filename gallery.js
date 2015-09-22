22e22050c30a996005029759b56e3d9f8ed11df2
blake <blakehunsicker@gmail.com>
2015-09-22 09:58:57 -0400

    fixing innerHTML

--- gallery.js
+++ gallery.js
@@ -1,3 +1,3 @@
 $('.headline-kicker').css('display','none');
-$('#article-topper h1').innerHTML('What is the Islamic State?');
-$('#wp-volt-gal').css('display','none').after('<h3>hello hello hello</h3>');
+$('#article-topper h1').innerHTML = 'What is the Islamic State?';
+$('#wp-volt-gal').css('display','none');
