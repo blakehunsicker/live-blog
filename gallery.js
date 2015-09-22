b41d00eb81d4d40ebe96195739324306fbaa75a9
blake <blakehunsicker@gmail.com>
2015-09-22 10:39:14 -0400

    adding p elements

--- gallery.js
+++ gallery.js
@@ -2,5 +2,5 @@ $(document).ready(function() {
 	$('.headline-kicker').css('display','none');
 	// $('#article-topper').children('h1').innerHTML = 'What is the Islamic State?';
 	$('.wp-volt-gal').css('display','none');
-	$("<span style='font-family:Georgia;font-size:18px;'>A gunman opened fire on beachgoers at a resort frequented by Europeans in what is widely seen as a campaign to devastate Tunisia's tourism industry.</span><span style='font-family:Georgia;font-size:18px;'>Many of the dead and injured were from Britain.</span>").appendTo('.pb-f-gallery-gallery');
+	$("<p style='font-size:18px;padding-right:10%;padding-left:10%;'>A gunman opened fire on beachgoers at a resort frequented by Europeans in what is widely seen as a campaign to devastate Tunisia's tourism industry.</p><p style='font-size:18px;padding-right:10%;padding-left:10%;'>Many of the dead and injured were from Britain.</p>").appendTo('.pb-f-gallery-gallery');
 });
