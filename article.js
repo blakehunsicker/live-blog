d0c8175de8a9d6070af901334b62d5f4b1429136
blake <blakehunsicker@gmail.com>
2015-09-22 11:33:33 -0400

    adding padding, article.js

---
+++ article.js
@@ -0,0 +1,5 @@
+$(document).ready(function() {
+	$('.pb-f-sharebars-utilities-tool-bar').css('display','none');
+	$('.pb-sig-line').css('display','none');
+
+});
--- gallery.js
+++ gallery.js
@@ -3,5 +3,5 @@ $(document).ready(function() {
 	$('h1').innerHTML = 'The massacre in Sousse';
 	$('.wp-volt-gal').css('display','none');
 	$('.pb-f-sharebars-utilities-tool-bar').css('display','none');
-	$("<p style='font-size:18px;width:60%;'>A gunman opened fire on beachgoers at a resort frequented by Europeans in what is widely seen as a campaign to devastate Tunisia's tourism industry.</p><p style='font-size:18px;width:60%;'>Many of the dead and injured were from Britain.</p>").appendTo('.pb-f-gallery-gallery');
+	$("<p style='font-size:18px;width:60%;padding-left:10px;'>A gunman opened fire on beachgoers at a resort frequented by Europeans in what is widely seen as a campaign to devastate Tunisia's tourism industry.</p><p style='font-size:18px;width:60%;padding-left:10px;'>Many of the dead and injured were from Britain.</p>").appendTo('.pb-f-gallery-gallery');
 });
