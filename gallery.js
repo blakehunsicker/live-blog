fc956dabd683d4f22c4dfbe320cd24acf5e19d76
blake <blakehunsicker@gmail.com>
2015-09-22 10:32:38 -0400

    adding Georgia

--- gallery.js
+++ gallery.js
@@ -2,5 +2,5 @@ $(document).ready(function() {
 	$('.headline-kicker').css('display','none');
 	// $('#article-topper').children('h1').innerHTML = 'What is the Islamic State?';
 	$('.wp-volt-gal').css('display','none');
-	$("<h4>A gunman opened fire on beachgoers at a resort frequented by Europeans in what is widely seen as a campaign to devastate Tunisia's tourism industry.</h4><h4>Many of the dead and injured were from Britain.</h4>").appendTo('.pb-f-gallery-gallery');
+	$("<span style='font-family:Georgia;font-size:18px;'>A gunman opened fire on beachgoers at a resort frequented by Europeans in what is widely seen as a campaign to devastate Tunisia's tourism industry.</span><span style='font-family:Georgia;font-size:18px;'>Many of the dead and injured were from Britain.</span>").appendTo('.pb-f-gallery-gallery');
 });
