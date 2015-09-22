6c26b16f7aae35d82ed903bffaf2387b9d65c715
blake <blakehunsicker@gmail.com>
2015-09-22 16:51:48 -0400

    fixing pb-feature

--- article.js
+++ article.js
@@ -9,7 +9,10 @@ $(document).ready(function() {
 	$('.pb-f-article-article-body .article-body').css('margin-top','0px');
 	$('.inline-content').css('display','none');
 	$('.pb-sig-line').css('display','none');
-	$('#pb-root .pb-feature').css('padding','0px 30px 10px 10px;')
+	$('#pb-root .pb-feature').css({
+		'padding': '0px 30px 10px 10px',
+		'border-right': 'none'
+	});
 	$('.flex-embed').css('display','none');
 	$('article').children().css('display','none');
 	$('#defaultImage').css('display','none');
