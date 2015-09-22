6d8d8a16463464a513d7f8ee4f4679e26ce923b6
blake <blakehunsicker@gmail.com>
2015-09-22 10:24:40 -0400

    adding text

--- gallery.js
+++ gallery.js
@@ -2,5 +2,5 @@ $(document).ready(function() {
 	$('.headline-kicker').css('display','none');
 	// $('#article-topper').children('h1').innerHTML = 'What is the Islamic State?';
 	$('.wp-volt-gal').css('display','none');
-	$('<h4>Hello hello hello</h4>').appendTo('.pb-f-gallery-gallery');
+	$("<h4>A gunman opened fire on beachgoers at a resort frequented by Europeans in what is widely seen as a campaign to devastate Tunisia's tourism industry.</h4><h4>Many of the dead and injured were from Britain.</h4>").appendTo('.pb-f-gallery-gallery');
 });
