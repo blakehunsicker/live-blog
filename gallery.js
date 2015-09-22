89118cf14e36b28e5ab3cabe36a9f5b60cf82353
blake <blakehunsicker@gmail.com>
2015-09-22 11:44:02 -0400

    adding more to sups-content

--- gallery.js
+++ gallery.js
@@ -1,8 +1,8 @@
 $(document).ready(function() {
 	// $('.headline-kicker').css('display','none');
 	$('#article-topper').children('h1').css('display','none');
-	$('<h1>The Massacre in Sousse</h1>').appendTo('#article-topper');
+	$('<h1>The attack in Sousse, Tunisia</h1>').appendTo('#article-topper');
 	$('.wp-volt-gal').css('display','none');
 	$('.pb-f-sharebars-utilities-tool-bar').css('display','none');
-	$("<p style='font-size:18px;width:60%;padding-left:10px;'>A gunman opened fire on beachgoers at a resort frequented by Europeans in what is widely seen as a campaign to devastate Tunisia's tourism industry.</p><p style='font-size:18px;width:60%;padding-left:10px;'>Many of the dead and injured were from Britain.</p>").appendTo('.pb-f-gallery-gallery');
+	$("<p style='font-size:18px;width:60%;padding-left:10px;'>A gunman opened fire on beachgoers at a resort frequented by Europeans in what is widely seen as a campaign to devastate Tunisia's tourism industry.</p><p style='font-size:18px;width:60%;padding-left:10px;'>Many of the dead and injured were from Britain.</p><p style='font-family:FranklinITCProBold;font-weight:bold;'>Read:</p><p style='color:#2e6d9d;'>Tunisia, after igniting the Arab Spring, sends the most fighters to Islamic State in Syria</p>").appendTo('.pb-f-gallery-gallery');
 });
