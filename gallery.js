f893f01a3cd2004675907a4a98329613a3c5e7bd
blake <blakehunsicker@gmail.com>
2015-09-22 09:56:46 -0400

    adding gallery.js

---
+++ gallery.js
@@ -0,0 +1,3 @@
+$('.headline-kicker').css('display','none');
+$('#article-topper h1').innerHTML('What is the Islamic State?');
+$('#wp-volt-gal').css('display','none').after('<h3>hello hello hello</h3>');
