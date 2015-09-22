4cd47fbb46ef9c33424501910c7828ea247f2b0c
blake <blakehunsicker@gmail.com>
2015-09-22 10:01:07 -0400

    getElement

--- gallery.js
+++ gallery.js
@@ -1,3 +1,5 @@
-$('.headline-kicker').css('display','none');
-$('#article-topper h1').innerHTML = 'What is the Islamic State?';
-$('#wp-volt-gal').css('display','none');
+$(document).ready(function() {
+	$('.headline-kicker').css('display','none');
+	document.getElementById('#article-topper h1').innerHTML = 'What is the Islamic State?';
+	$('#wp-volt-gal').css('display','none');
+});
