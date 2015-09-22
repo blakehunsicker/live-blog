dce74dde7b0236f29b6263f2d92104e16498dc57
blake <blakehunsicker@gmail.com>
2015-09-22 14:16:46 -0400

    adding supp content

--- article.js
+++ article.js
@@ -5,5 +5,5 @@ $(document).ready(function() {
 	$('.pb-sig-line').css('display','none');
 	$('.flex-embed').css('display','none');
 	$('article').children().css('display','none');
-
+	$("<div style='width:60%;height:300px;background-color:#E7E7E7;margin-left:10px;'></div><div style='width:60%;font-family:FranklinITCProLight;line-height:1.1rem;font-size:15px;color:#999;padding-left:10px;margin-top:5px;margin-bottom:10px;'>Police officers control the crowd (rear) while surrounding a man (front center) suspected to be involved the attack on a beach in Sousse, Tunisia. (Amine Ben Aziza/Reuters)</div><p style='font-size:18px;width:60%;padding-left:10px;'>A gunman opened fire on beachgoers at a resort frequented by Europeans in what is widely seen as a campaign to devastate Tunisia's tourism industry.</p><p style='font-size:18px;width:60%;padding-left:10px;'>Many of the dead and injured were from Britain.</p><p style='font-family:FranklinITCProBold;font-weight:bold;padding-left:10px;margin-bottom:10px;'>Read:</p><p style='width:60%;font-family:FranklinITCProLight;color:#2e6d9d;padding-left:10px;'>Tunisia, after igniting the Arab Spring, sends the most fighters to Islamic State in Syria</p><p style='width:60%;font-family:FranklinITCProLight;color:#2e6d9d;padding-left:10px;'>Sousse: The beautiful Tunisian city that became a scene of terror</p>").appendTo('article');
 });
