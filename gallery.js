75f5c6ddfc92a3aff0e6272c003996e1d77071b5
blake <blakehunsicker@gmail.com>
2015-09-22 12:13:13 -0400

    changing padding

--- gallery.js
+++ gallery.js
@@ -1,9 +1,9 @@
 $(document).ready(function() {
 	$('#article-topper').css('padding-left','20%');
-	$('.pb-f-gallery-gallery').css('padding-left','20%');
+	// $('.pb-f-gallery-gallery').css('padding-left','20%');
 	$('#article-topper').children('h1').css('display','none');
 	$('<h1>The attack in Sousse, Tunisia</h1>').appendTo('#article-topper');
 	$('.wp-volt-gal').css('display','none');
 	$('.pb-f-sharebars-utilities-tool-bar').css('display','none');
-	$("<p style='font-size:18px;width:60%;padding-left:10px;'>A gunman opened fire on beachgoers at a resort frequented by Europeans in what is widely seen as a campaign to devastate Tunisia's tourism industry.</p><p style='font-size:18px;width:60%;padding-left:10px;'>Many of the dead and injured were from Britain.</p><p style='font-family:FranklinITCProBold;font-weight:bold;padding-left:10px;'>Read:</p><p style='width:60%;font-family:FranklinITCProLight;color:#2e6d9d;padding-left:10px;'>Tunisia, after igniting the Arab Spring, sends the most fighters to Islamic State in Syria</p>").appendTo('.pb-f-gallery-gallery');
+	$("<p style='font-size:18px;width:60%;padding-left:20%;'>A gunman opened fire on beachgoers at a resort frequented by Europeans in what is widely seen as a campaign to devastate Tunisia's tourism industry.</p><p style='font-size:18px;width:60%;padding-left:20%;'>Many of the dead and injured were from Britain.</p><p style='font-family:FranklinITCProBold;font-weight:bold;padding-left:20%;'>Read:</p><p style='width:60%;font-family:FranklinITCProLight;color:#2e6d9d;padding-left:20%;'>Tunisia, after igniting the Arab Spring, sends the most fighters to Islamic State in Syria</p>").appendTo('.pb-f-gallery-gallery');
 });
