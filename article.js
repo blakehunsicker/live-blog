0d2dc1ca1479a2ce6cbb9a84b681b738b3c54ad2
blake <blakehunsicker@gmail.com>
2015-09-24 10:40:34 -0400

    adding timestamp

--- article.js
+++ article.js
@@ -7,7 +7,7 @@ $(document).ready(function() {
 	});
 	$('#article-topper').children('h1').css('display','none');
 	$('<h1 class="new-title">The attack in Sousse, Tunisia (what would this look like after a line break?)</h1>').appendTo('#article-topper');
-	$('<div class="supplement-tag" style="margin-bottom:20px;"><span style="display:inline-block;background-color:#DCA48D;border-radius:1em;padding:6px;margin-right:8px;"></span><span style="font-family:FranklinITCProBold;font-size:16px;">Topic Index</span></div>').insertAfter('.new-title');
+	$('<div class="supplement-tag" style="margin-bottom:20px;"><span style="display:inline-block;background-color:#DCA48D;border-radius:1em;padding:6px;margin-right:8px;"></span><span style="font-family:FranklinITCProBold;font-size:16px;">Topic Index <span style="color:#E0E0E0;">Specific background information</span></span></div>').insertAfter('.new-title');
 	// $('.pb-f-sharebars-utilities-tool-bar').css('display','none');
 	$('.pb-f-graphic-graphic-main .graphic-main').css({
 		'padding-top': '30px',
@@ -30,5 +30,6 @@ $(document).ready(function() {
 	$('.wp-inline-bb.pb-centered-bb').css('display','none');
 	$('#google_image_div').css('display','none');
 	$('.post-body-sig-line').css('display','none');
+	$("<span class='pb-timestamp'>September 24 at 10:29 AM</span>").appendTo('article');
 	$("<div style='height:300px;background-color:#E7E7E7;'></div><div style='font-family:FranklinITCProLight;line-height:1.1rem;font-size:15px;color:#999;margin-top:5px;margin-bottom:10px;'>Police officers control the crowd (rear) while surrounding a man (front center) suspected to be involved the attack on a beach in Sousse, Tunisia. (Amine Ben Aziza/Reuters)</div><div class='inline-content' style='padding:0px;border-top:none;'></div><p style='font-size:20px;'>A gunman opened fire on beachgoers at a resort frequented by Europeans in what is widely seen as a campaign to devastate Tunisia's tourism industry.</p><p style='font-size:20px;'>Many of the dead and injured were from Britain.</p><p style='font-family:FranklinITCProBold;font-weight:bold;margin-bottom:10px;'>Read:</p><p style='font-family:FranklinITCProLight;line-height:20px;color:#2e6d9d;'>Tunisia, after igniting the Arab Spring, sends the most fighters to Islamic State in Syria</p><p style='font-family:FranklinITCProLight;line-height:20px;color:#2e6d9d;'>Sousse: The beautiful Tunisian city that became a scene of terror</p>").appendTo('article');
 });
