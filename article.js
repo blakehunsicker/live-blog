d3858f4a716fc73a37d03a243cc236e8c8be61e4
blake <blakehunsicker@gmail.com>
2015-09-29 13:28:02 -0400

    fixing

--- article.js
+++ article.js
@@ -18,9 +18,9 @@ $(document).ready(function() {
 		}
 	);
 	
-	$('.pb-1').css({
-		'margin-top': '100px !important',
-		'border-top': '1px solid #D5D5D5 !important'
+	$('.pb-3.pb-f-graphic-graphic-main').css({
+		'padding-bottom': '100px !important',
+		'border-bottom': '1px solid #D5D5D5 !important'
 	});
 	$('.pb-f-graphic-graphic-main .graphic-main').css({
 		'padding-top': '30px',
